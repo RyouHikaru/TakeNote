7aa2b8b4cd9ae23f6d4a8296d0d3bb55f5cf9a0e
RyouHikaru <carljamesreblora13@gmail.com>
2020-02-15 21:24:50 +0800

    User Profile Fragment updated.

--- app/src/main/java/com/example/navigationdrawerdemo/ui/user_profile/UserProfileFragment.java
+++ app/src/main/java/com/example/navigationdrawerdemo/ui/user_profile/UserProfileFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -23,13 +22,7 @@ public class UserProfileFragment extends Fragment {
         upViewModel =
                 ViewModelProviders.of(this).get(UserProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_user_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
-        upViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
