4680e66e453c321a3a2a93f84956fb4f8fee2477
RyouHikaru <carljamesreblora13@gmail.com>
2020-02-23 21:39:54 +0800

    Programmatically enable addition of TextViews.

--- app/src/main/java/com/example/takenote/ui/notes/NotesFragment.java
+++ app/src/main/java/com/example/takenote/ui/notes/NotesFragment.java
@@ -1,43 +1,99 @@
 package com.example.takenote.ui.notes;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.takenote.R;
 
 public class NotesFragment extends Fragment {
-
+    private View root;
+    private TextView item1TextView, item2TextView, item3TextView;
     private NotesViewModel notesViewModel;
+    private View.OnClickListener notesListener;
+    private LinearLayout linearLayout;
+    private LinearLayout.LayoutParams layout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notesViewModel =
                 ViewModelProviders.of(this).get(NotesViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notes, container, false);
+//        root = inflater.inflate(R.layout.fragment_notes, container, false);
+        final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.NoteFragmentStyle);
+        LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
+        root = localInflater.inflate(R.layout.fragment_notes, container, false);
+        item1TextView = root.findViewById(R.id.item1textView);
+        item2TextView = root.findViewById(R.id.item2TextView);
+        item3TextView = root.findViewById(R.id.item3TextView);
 
-        final TextView textView = root.findViewById(R.id.text_note);
-
-        notesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        notesListener = new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                showItemDialog(((TextView) v).getText().toString());
+            }
+        };
+
+        item1TextView.setOnClickListener(notesListener);
+        item2TextView.setOnClickListener(notesListener);
+        item3TextView.setOnClickListener(notesListener);
+
+        // region Programmatically added TextViews
+        linearLayout = root.findViewById(R.id.notes_linear_layout);
+        layout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 140);
+        layout.setMargins(40, 40, 40, 40);
+
+        addTextView();
+        // endregion
+        return root;
+    }
+    public void showItemDialog(String title) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(title);
+        builder.setMessage("This will display the content from your note");
+
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return;
             }
         });
 
-        final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.NoteFragmentStyle);
-        LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
+        builder.setPositiveButton("Edit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "This will go to EditActivity", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        });
 
-        return localInflater.inflate(R.layout.fragment_notes, container, false);
+        AlertDialog itemDialog = builder.create();
+        itemDialog.show();
+    }
+    public void addTextView() {
+        TextView[] items = new TextView[5];
+        for (int i = 0; i < 5; i++) {
+            items[i] = new TextView(getActivity());
+            items[i].setPaddingRelative(50, 40, 50, 40);
+            items[i].setBackgroundResource(R.drawable.notes_selector);
+            items[i].setText("Another one");
+            items[i].setTextSize(20);
+            items[i].setVisibility(View.VISIBLE);
+            items[i].setLayoutParams(layout);
+            items[i].setOnClickListener(notesListener);
+            linearLayout.addView(items[i]);
+        }
     }
 }
