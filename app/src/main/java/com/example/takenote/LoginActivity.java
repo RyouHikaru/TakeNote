ebecb3ad47a40aae5be787b353d993554312c4d8
RyouHikaru <carljamesreblora13@gmail.com>
2020-02-29 23:07:36 +0800

    Phase 2 of v3.0. Added some friendly notifications. Working settings.

--- app/src/main/java/com/example/takenote/LoginActivity.java
+++ app/src/main/java/com/example/takenote/LoginActivity.java
@@ -1,5 +1,9 @@
 package com.example.takenote;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,26 +15,24 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 
 public class LoginActivity extends AppCompatActivity {
     // region Object and Variables
-    private static int TIME_DELAY = 750;
+    private static int TIME_DELAY = 600;
     private Button loginButton, cancelButton;
     private TextView signUpView;
     private EditText unEditText, pwEditText;
     private static Intent intent;
     private static TakeNoteDatabase myDb;
+    private static int[] settings;
     // endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        if(getIntent().getBooleanExtra("EXIT", false)) {
-            finish();
-            System.exit(0);
-        }
 
         // region XML mapping
         loginButton = findViewById(R.id.loginButton);
@@ -39,6 +41,7 @@ public class LoginActivity extends AppCompatActivity {
         unEditText = findViewById(R.id.unEditText);
         pwEditText = findViewById(R.id.pwEditText);
         myDb = new TakeNoteDatabase(this);
+        settings = myDb.getSettings();
         // endregion
 
         // region loginButton listener
@@ -65,9 +68,28 @@ public class LoginActivity extends AppCompatActivity {
 
                     if (pw.equals(pwKey)) {
                         Toast.makeText(LoginActivity.this, "Logging in", Toast.LENGTH_SHORT).show();
+
                         new Handler().postDelayed(new Runnable() {
                             @Override
                             public void run() {
+                                if (settings[1] == 1) {
+                                    // region TESTING INTENT FOR NOTIFICATION
+                                    Intent i = new Intent();
+                                    PendingIntent pi = PendingIntent.getActivity(LoginActivity.this, 70, i, PendingIntent.FLAG_UPDATE_CURRENT);
+                                    Notification.Builder builder = new Notification.Builder(LoginActivity.this);
+                                    builder.setSmallIcon(R.drawable.ic_notifications_white_24dp)
+                                            .setContentTitle("WELCOME BACK!")
+                                            .setContentText("What do you have to share?")
+                                            .setWhen(System.currentTimeMillis())
+                                            .setAutoCancel(true)
+                                            .setContentIntent(pi)
+                                            .setPriority(Notification.PRIORITY_MAX)
+                                            .setDefaults(Notification.DEFAULT_ALL);
+                                    NotificationManager nm = (NotificationManager) getApplication().getSystemService(Context.NOTIFICATION_SERVICE);
+                                    nm.notify(0, builder.build());
+                                    // endregion
+                                }
+
                                 intent = new Intent(LoginActivity.this, MainActivity.class);
                                 intent.putExtra("UN", un);
                                 startActivity(intent);
@@ -89,6 +111,22 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(LoginActivity.this, "Closing", Toast.LENGTH_SHORT).show();
+
+                if (settings[1] == 1) {
+                    Intent i = new Intent(LoginActivity.this, WelcomeActivity.class);
+                    PendingIntent pi = PendingIntent.getActivity(LoginActivity.this, 69, i, PendingIntent.FLAG_UPDATE_CURRENT);
+                    Notification.Builder builder = new Notification.Builder(LoginActivity.this);
+                    builder.setSmallIcon(R.drawable.ic_notifications_white_24dp)
+                            .setContentTitle("You can comeback anytime!")
+                            .setContentText("Click to Login. Swipe to ignore.")
+                            .setWhen(System.currentTimeMillis())
+                            .setAutoCancel(true)
+                            .setContentIntent(pi)
+                            .setPriority(Notification.PRIORITY_MAX)
+                            .setDefaults(Notification.DEFAULT_ALL);
+                    NotificationManager nm = (NotificationManager) getApplication().getSystemService(Context.NOTIFICATION_SERVICE);
+                    nm.notify(0, builder.build());
+                }
                 finish();
             }
         });
